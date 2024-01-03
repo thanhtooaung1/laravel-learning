68298cee096382bce17909a94e4fb41b262d67b2
thanhtooaung1 <thanhtooaung@gicjp.com>
2024-01-03 16:07:58 +0630

    database setup and create, migrate, model, factory, seeder, laravel ui with auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 
 Route::resource('test', TestController::class);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
